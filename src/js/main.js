26b14505eabeda1a3a90ad528063fcad0824e4dd
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-05-15 22:30:31 -0600

    up to date

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+    console.log("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸Hello, Developer! Thanks for looking at my portfolio¸,ø¤º°`°º¤ø,¸¸,ø¤º°¤ø,¸,ø¤º°`°º¤ø,¸¸,ø¤º°");
+    console.log("                            __");
+    console.log("     ,                    ,\" e`--o");
+    console.log("    ((                   (  | __,\'");
+    console.log("     \\~----------------' \\_;/");
+    console.log("     (                      /");
+    console.log("     /) ._______________.  )");
+    console.log("    (( (               (( (");
+    console.log("     ``-'               ``-'");
 });
+
+
